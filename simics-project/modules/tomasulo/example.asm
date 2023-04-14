6f8a4aa4a1f5acf32e082e4da2f33ad75267d1fd
Aykull <31603222+Aykull@users.noreply.github.com>
2023-04-14 17:54:40 -0600

    Complete x86 example

--- simics-project/modules/tomasulo/example.asm
+++ simics-project/modules/tomasulo/example.asm
@@ -1,25 +1,16 @@
-ORG 0x0fffffff0           ; Offset
+ORG 0x0fffffff0           ; add to offsets
 
 start:
-   ;mov ax, 0x0000 
-   ;mov ds, ax
-   ;mov ax, 0x1000
-   ;mov ss, ax
-
-   mov ax, 0   ; primer número de Fibonacci
-   mov bx, 1   ; segundo número de Fibonacci
-   mov cx, 10   ; número de términos a calcular
+   mov al, 0   ; primer número de Fibonacci
+   mov bl, 1   ; segundo número de Fibonacci
+   mov cl, 5   ; número de términos a calcular
    ; Ciclo para calcular los términos de Fibonacci
    fibonacci:
-      ; Calcula el siguiente término de Fibonacci
-      add ax, bx
-      mov dx, bx
-      ;mov bx, ax
-      ;mov ax, dx 
-      dec cx
+      add al, bl
+      mov bl, al
+      sub al, bl
+      dec cl
       jnz fibonacci
-    
-   ; Termina el programa
 
 section .data
    fib: db 0, 1, 1, 2, 3, 5, 8, 13, 21, 34

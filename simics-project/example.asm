ba7ebf0a301f34f0327f7053ee9b53b875a5b53f
Maria Monge Golcher <maria.monge.golcher@intel.com>
2023-08-15 14:22:37 -0700

    Linear-feedback shift register workshop

--- simics-project/example.asm
+++ simics-project/example.asm
@@ -1,9 +1,17 @@
-ORG 0x0           ; add to offsets
+ORG 0x0                 ; add to offsets
 
-start:
-    mov eax, 3
-    add eax, eax
-    mov eax, 5
-    mov ebx, 6
-    add eax, ebx
-    add ebx, eax
+start:  
+    mov eax, 0          ; primer número de fibonacci
+    mov ebx, 1          ; segundo número de fibonacci
+    mov ecx, 10         ; número de términos a calcular
+
+    fibonacci:          ; ciclo para calcular los términos de Fibonacci
+        mov edx, eax
+        mov eax, ebx
+        add eax, edx    ; numeros de fibonacci ciclo a ciclo (observable en Simics Control Menu -> Debug -> CPU Registers)
+        mov ebx, edx    
+        dec ecx
+        jnz fibonacci
+
+section .data
+    fib: db 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
